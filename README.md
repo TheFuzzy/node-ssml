c82d855244898bfc3130a2660deaa50136c7cfb1
TheFuzzy <Infestor51@gmail.com>
2014-01-15 12:21:57 +0800

    Initial commit for node-ssml v0.0.2

---
+++ README.md
@@ -0,0 +1,345 @@
+# node-ssml
+[![NPM](https://nodei.co/npm/ssml.png)](https://nodei.co/npm/ssml/)
+
+A Node.js library for producing SSML (Speech Synthesis Markup Language) according to specifications.
+
+Currently, this library only implements [SSML 1.0](http://www.w3.org/TR/speech-synthesis/).
+The documentation also assumes that the reader has at least *skimmed* through the specification.
+
+As of v0.0.2, only the following elements of SSML are supported:
+
+* say-as
+* prosody
+* audio
+* break
+* sub
+
+## Basic Usage
+
+This library makes use of method chaining for convenience.
+
+```javascript
+var ssml = require('ssml');
+var ssmlDoc = new ssml();
+
+ssmlDoc.say('This is a great voice application!')
+    .break(500)
+    .prosody({ rate: '0.8' })
+    .say('Awkward pause')
+    .toString({ pretty: true });
+```
+
+Output:
+```xml
+<?xml version="1.0">
+<speak xmlns="http://www.w3.org/2001/10/synthesis" version="1.0" xml:lang="en-US">
+	This is a great voice application!
+	<break time="500ms"/>
+	<prosody rate="0.8">Awkward pause</prosody>
+</speak>
+```
+
+XML output examples in the later section assume `pretty` is set to true for the `toString()` function (described later), for easier reading.
+
+## Initialization
+
+Simply initialize a document instance as follows:
+
+```javascript
+var ssml = require('ssml');
+...
+var ssmlDoc = new ssml()
+```
+
+The document is initialized with language set to `en-US` by default. To use a different language, specify it in the `lang` parameter:
+
+```javascript
+var ssmlDoc = new ssml({ lang: 'fr-FR' });
+...
+ssmlDoc.toString()
+```
+Output:
+
+```xml
+<?xml version="1.0">
+<speak xmlns="http://www.w3.org/2001/10/synthesis" version="1.0" xml:lang="fr-FR">
+...
+```
+
+## Elements
+
+Note that all functions, including elements, are object-based, and not static.
+
+### say(text[, options]) / say(options)
+
+Produces basic text output. Usage is as follows:
+
+```javascript
+ssmlDoc.say('I am talking to you.');
+```
+
+The text string can also be described by the `text` parameter.
+
+`intepretAs` can also be specified as an additional parameter, but will encapsulate the text-output into a `say-as` element. More parameters can be specified for the `say-as` element, only *if* `intepretAs` is used:
+
+* `format`
+* `detail`
+
+Their respective purposes can be found in [the specification](http://www.w3.org/TR/ssml-sayas/).
+
+Example:
+
+```javascript
+ssmlDoc.say('The time now is ')
+    .say({
+        text: '01:59:59',
+        intepretAs: 'telephone',
+        format: 'hms24'
+    })
+    .toString();
+```
+
+Output:
+
+```xml
+<?xml version="1.0">
+<speak xmlns="http://www.w3.org/2001/10/synthesis" version="1.0" xml:lang="en-US">
+	The time now is 
+	<say-as interpret-as="time" format="hms24">01:59:59</say-as>
+</speak>
+```
+
+### break(time[, options]) / break(options)
+
+Represents a pause in the speech. Accepts one of the following parameters:
+
+* `time` - Can be a string or number. If passed as a number, it will be converted to a string automatically, and appended with `ms`. Will override `strength` if defined.
+* `strength` - A string value that describes the length of time in a human-readable form i.e. `weak`, `medium`, `strong`.
+
+`time` may also be defined as the first parameter of the `break` function for convenience, i.e.:
+
+```javascript
+.break(500) // 500ms
+```
+
+Additional information on these parameters can be found in [the specification](http://www.w3.org/TR/speech-synthesis/#edef_break).
+
+Example:
+
+```javascript
+ssmlDoc.say('It\'s time for an awkward pause!')
+    .break({
+        time: '5s',
+        strength: 'strong'
+    })
+    .toString();
+```
+
+Output:
+
+```xml
+<?xml version="1.0">
+<speak xmlns="http://www.w3.org/2001/10/synthesis" version="1.0" xml:lang="en-US">
+	It's time for an awkward pause!
+	<break time="5s"/>
+</speak>
+```
+
+### prosody(options)
+
+Represents a speech adjustment. Any element contained in a `prosody` element will receive the adjustments described by its parameters.
+
+`prosody` also switches the internal context of the SSML document, so that further element calls will render elements within the `prosody` element. Use `up()`(covered later) to reset the context to the previous level.
+
+Accepts any of the following parameters, alone or in combination:
+
+* `pitch` - A string or number that controls the pitch of the spoken text. If a number is supplied, `Hz` will be appended to the output.
+* `rate` - A string or number that controls the speed of the spoken text.
+* `range` - A string or number that controls the pitch range of the spoken text. If a number is supplied, `Hz` will be appended to the output.
+* `volume` - A string or number that controls the volume of the spoken text.
+* `contour` - a string containing a set of values to finely tune the pitch of the spoken text.
+
+Additional information on these parameters can be found in [the specification](http://www.w3.org/TR/speech-synthesis/#edef_prosody).
+
+Example:
+
+```javascript
+// Produce a sarcastic apology.
+ssmlDoc.say('I\'m ')
+    .prosody({
+        rate: '0.6',
+        pitch: '-50%'
+    })
+    .say('sorry')
+    .toString();
+```
+
+Output:
+
+```xml
+<?xml version="1.0">
+<speak xmlns="http://www.w3.org/2001/10/synthesis" version="1.0" xml:lang="en-US">
+	I'm 
+	<prosody pitch="-50% rate="0.6">sorry</prosody>
+</speak>
+```
+
+### audio(src[, options]) / audio(options)
+
+Represents an audio resource. Accepts the following parameters:
+
+* `src` or `source` - The URI pointing to a valid audio resource. Required.
+* `alt` - A string that is rendered by the speech processor if the audio resource cannot be found. Optional.
+
+`src` may also be defined as the first parameter of the `audio` function for convenience, i.e.:
+
+```javascript
+.audio('http://yourdomain.com/audio.mp3')
+```
+
+Additional information on these parameters can be found in [the specification](http://www.w3.org/TR/speech-synthesis/#edef_audio).
+
+Example:
+
+```javascript
+// Rickroll the guy.
+ssmlDoc.audio({
+        src: 'http://music.com/Never Gonna Give You Up.mp3',
+        alt: 'For some reason that didn\'t work. Oh well.'
+    })
+    .toString();
+```
+
+Output:
+
+```xml
+<?xml version="1.0">
+<speak xmlns="http://www.w3.org/2001/10/synthesis" version="1.0" xml:lang="en-US">
+	<audio src="http://music.com/Never Gonna Give You Up.mp3">For some reason that didn't work. Oh well.</audio>
+</speak>
+```
+
+## Helper functions
+
+### up()
+
+This function sets the internal context to the previous level in XML. If there is no previous level, the function does nothing.
+
+Note that as of v0.0.2, this is a mutable operation affecting the SSML document object, and cannot be reversed.
+
+Example:
+
+```javascript
+// Mock the user
+ssmlDoc.say('Now, now.')
+    .prosody({ rate: '0.8' })
+    .say('There\'s no need to be upset.')
+    .up()
+    .say('We\'ll be just fine!')
+    .toString();
+```
+
+Output:
+
+```xml
+<?xml version="1.0">
+<speak xmlns="http://www.w3.org/2001/10/synthesis" version="1.0" xml:lang="en-US">
+	Now, now.
+	<prosody rate="0.8">There's no need to be upset.</prosody>
+	We'll be just fine!
+</speak>
+```
+
+### clear()
+
+This function clears all elements from the SSML document object.
+
+### replace(wordToReplace, replaceWith) / replace(keyValues)
+
+This function replaces all instances of the word(s) with another word/set of words. These can be passed in as the first and second parameters for a single key-value pair, or as an object containing key-value pairs. The words will then be added to an internal dictionary and processed only when `toString()` is called.
+
+Note that the `replace` function is case-sensitive, and does not modify the original text.
+
+The replaced words will still be rendered, but wrapped in a `sub` element. Additional information about this element can be found in [the specification](http://www.w3.org/TR/speech-synthesis/#edef_sub).
+
+Example:
+
+```javascript
+ssmlDoc.replace('balls of fire', 'suns above')
+    .say('Great balls of fire! The time has arrived, and thus have I!')
+    .addReplace({
+        'arrived' : 'come',
+        'thus' : 'so'
+    })
+    .say('Thus, we\'ll commence the operation!')
+    .toString();
+```
+
+Output:
+
+```xml
+<?xml version="1.0">
+<speak xmlns="http://www.w3.org/2001/10/synthesis" version="1.0" xml:lang="en-US">
+	Great <sub alias="suns above">balls of fire</sub>! The time has <sub alias="come">arrived</sub>, and <sub alias="so">thus</sub> have I!
+	And <sub alias="so">thus</sub>, we'll commence the operation!
+</speak>
+```
+
+### addReplace(wordToReplace, replaceWith) / replace(keyValues)
+
+Alias for `replace`.
+
+### removeReplace(key)
+
+This function removes the key from the internal dictionary. If an array of strings is provided, all keys matching the elements in the array will be removed.
+
+### clearReplace()
+
+Clears all keys from the internal dictionary.
+
+## Output
+
+### toString(options)
+
+Renders the SSML document as a string, which can be passed to the speech processor through a suitable medium later.
+
+Accepts the following parameters:
+
+* `minimal` - setting this to true renders a `speak` XML root element with no properties (including required properties such as `xml:lang`) or XML header. Takes priority over `full`.
+* `full` - setting this to true renders a `speak` XML root element with required *and* optional properties.
+
+Additionally, as the `xmlbuilder` library is being used, the options object may also contain any parameters suitable for the `toString()` method, such as `pretty` or `indent`. Refer to the `xmlbuilder` [documentation](https://github.com/oozcitak/xmlbuilder-js/wiki) for details.
+
+Examples:
+
+```javascript
+ssmlDoc.toString();
+```
+
+Output:
+
+```xml
+<?xml version="1.0">
+<speak xmlns="http://www.w3.org/2001/10/synthesis" version="1.0" xml:lang="en-US">Hello there!</speak>
+```
+
+```javascript
+ssmlDoc.toString({ full:true, minimal: true });
+```
+
+Output:
+
+```xml
+<speak>Hello there!</speak>
+```
+
+```javascript
+ssmlDoc.toString({ full:true });
+```
+
+Output:
+
+```xml
+<?xml version="1.0">
+<speak xmlns="http://www.w3.org/2001/10/synthesis" version="1.0" xml:lang="en-US" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.w3.org/TR/speech-synthesis/synthesis.xsd">Hello there!</speak>
+```
---
+++ index.js
@@ -0,0 +1,369 @@
+/********************************************************************
+ * @module node-ssml
+ * @version 0.0.2
+ * This is a rudimentary implementation of an SSML library.
+ * 
+ * This library is able to produce an SSML document readable by a
+ * SSML-compliant processor.
+ * This version only implements a very small subset of the SSML 1.0
+ * specification, described at http://www.w3.org/TR/speech-synthesis/
+ *
+ *******************************************************************/
+/**
+ * Module dependencies
+ */
+var xmlBuilder = require('xmlbuilder');
+var _ = require('underscore');
+
+/**
+ * @constructor
+ * @this {SSML}
+ * @param {Object} Constructor options, as follows:
+ *                  * language: The main language of the SSML document. Default is 'en-US'.
+ */
+function SSML(options) {
+    if (!(this instanceof SSML))
+        return new SSML(options);
+    this._elements = [];
+    this._replace = {};
+    this._options = {
+        language: 'en-US'
+    };
+    if (options) {
+        // TODO: Absorb more constructor options.
+        _.extend(this._options, _.pick(options, 'language'));
+    }
+}
+
+function isInvalidNumber(property) {
+    return _.isNumber(property) && !_.isFinite(property);
+}
+
+function isInvalid(property) {
+    return property && !_.isString(property);
+}
+
+function Say(options) {
+    if (!(this instanceof Say))
+        return new Say(options);
+    if (options) {
+        _.extend(this, _.pick(options, 'text', 'intepretAs', 'format', 'detail'));
+    }
+}
+
+Say.prototype.isValid = function isSayValid() {
+    if (!_.isString(this.text)) return false;
+    if (isInvalid(this.intepretAs)) return false;
+    return true;
+}
+
+Say.prototype.renderInto = function renderSayInto(xml, replacer) {
+    var outputText = this.text;
+    var outputQueue = [];
+    for (var replacedText in replacer) {
+        if (!_.has(replacer, replacedText)) continue;
+        var subElement = xmlBuilder.create('sub', null, null, { headless: true });
+        subElement.att('alias', replacer[replacedText]).txt(replacedText);
+        outputText = outputText.replace(new RegExp(replacedText, 'g'), subElement.end());
+    }
+    if (this.intepretAs) {
+        var sayAsElement = xml.ele('say-as');
+        sayAsElement.raw(outputText);
+        sayAsElement.att('intepret-as', this.intepretAs);
+        if (_.isString(this.format))
+            sayAsElement.att('format', this.format);
+        if (_.isString(this.detail))
+            sayAsElement.att('detail', this.detail);
+    } else {
+        xml.raw(outputText);
+    }
+    return xml;
+}
+
+function Break(options) {
+    if (!(this instanceof Break))
+        return new Break(options);
+    if (options) {
+        _.extend(this, _.pick(options, 'time', 'strength'));
+    }
+}
+
+Break.prototype.isValid = function isBreakValid() {
+    if (isInvalidNumber(this.time) && isInvalid(this.time)) return false;
+    if (isInvalid(this.strength)) return false;
+    return true;
+}
+
+Break.prototype.renderInto = function renderBreakInto(xml) {
+    var breakElement = xml.ele('break');
+    // Time takes priority over strength.
+    if (this.time) {
+        var timeValue = this.time;
+        if (_.isNumber(this.time)) timeValue = this.time + "ms";
+        breakElement.att('time', timeValue);
+    }
+    else if (this.strength) breakElement.att('strength', this.strength);
+    return xml;
+}
+
+function Prosody(options) {
+    if (!(this instanceof Prosody))
+        return new Prosody(options);
+    if (options) {
+        _.extend(this, _.pick(options, 'pitch', 'contour', 'range', 'rate', 'duration', 'volume'));
+    }
+}
+
+Prosody.prototype.isValid = function isProsodyValid() {
+    if (isInvalidNumber(this.pitch) && isInvalid(this.pitch)) return false;
+    if (isInvalid(this.contour)) return false;
+    if (isInvalidNumber(this.range) && isInvalid(this.range)) return false;
+    if (isInvalidNumber(this.rate) && isInvalid(this.rate)) return false;
+    if (isInvalidNumber(this.duration) && isInvalid(this.duration)) return false;
+    if (isInvalidNumber(this.volume) && isInvalid(this.volume)) return false;
+    return true;
+}
+
+Prosody.prototype.renderInto = function renderProsodyInto(xml) {
+    var prosodyElement = xml.ele('prosody');
+    if (this.pitch) {
+        var pitchValue = this.pitch;
+        if (_.isNumber(this.pitch)) pitchValue = this.pitch + "Hz";
+        prosodyElement.att('pitch', pitchValue);
+    }
+    if (this.contour) prosodyElement.att('contour', this.contour);
+    if (this.range) {
+        var rangeValue = this.range;
+        if (_.isNumber(this.range)) rangeValue = this.range + "Hz";
+        prosodyElement.att('range', rangeValue);
+    }
+    if (this.rate) prosodyElement.att('rate', this.rate);
+    if (this.duration) {
+        var durationValue = this.duration;
+        if (_.isNumber(this.duration)) durationValue = this.duration + "ms";
+        prosodyElement.att('duration', durationValue);
+    }
+    if (this.volume) prosodyElement.att('volume', this.volume);
+    return prosodyElement;
+}
+
+function Audio(options) {
+    if (!(this instanceof Audio))
+        return new Audio(options);
+    if (options) {
+        options.src = options.src || options.source;
+        _.extend(this, _.pick(options, 'src', 'alt'));
+    }
+}
+
+Audio.prototype.isValid = function isAudioValid() {
+    if (isInvalid(this.src)) return false;
+    if (isInvalid(this.alt)) return false;
+    return true;
+}
+
+Audio.prototype.renderInto = function renderAudioInto(xml) {
+    var audioElement = xml.ele('audio');
+    if (this.src) audioElement.att('src', this.src);
+    if (this.alt) audioElement.txt(this.alt);
+    return xml;
+}
+
+function EndElement() {
+    if (!(this instanceof EndElement))
+        return new EndElement();
+}
+
+EndElement.prototype.isValid = function isEndElementValid() {
+    return true;
+}
+
+EndElement.prototype.renderInto = function renderEndElementInto(xml) {
+    if (xml.isRoot) return xml.up();
+    return xml;
+}
+
+// Template
+// SSML.prototype.action = function action (stuff) {
+//     ...
+//     return this;
+// }
+
+/**
+ * @method say
+ * @param {String} The text (or object with toString()) to be output. Optional.
+ * @param {Object} An object containing options.
+ * @throws {Error} The options provided are invalid.
+ */
+SSML.prototype.say = function say(text, options) {
+    if (_.isObject(text) && !_.isObject(options)) {
+        options = text;
+    } else if (!options) {
+        options = {};
+    }
+    if (_.isString(text)) {
+        options.text = text;
+    } else if (_.isFunction(text.toString)) {
+        options.text = text.toString();
+    }
+    var newSay = new Say(options);
+    if (!newSay.isValid) throw new Error("Say has invalid options!");
+    this._elements.push(newSay);
+    
+    return this;
+}
+
+/**
+ * @method break
+ * @param {String/Number} A string (or number) representing the amount of time. Optional.
+ * @param {Object} An object containing options.
+ * @throws {Error} The options provided are invalid.
+ */
+SSML.prototype.break = function ssmlBreak(time, options) {
+    if (_.isObject(time) && !_.isObject(options)) {
+        options = time;
+    } else if (!options) {
+        options = {};
+    }
+    if (_.isFinite(time) || _.isString(time)) {
+        options.time = time;
+    }
+    var newBreak = new Break(options);
+    if (!newBreak.isValid) throw new Error("Break has invalid options!");
+    this._elements.push(newBreak);
+    return this;
+}
+
+/**
+ * @method prosody
+ * @param {Object} An object containing options.
+ * @throws {Error} The options provided are invalid.
+ */
+SSML.prototype.prosody = function prosody(options) {
+    var newProsody = new Prosody(options);
+    if (!newProsody.isValid) throw new Error("Prosody has invalid options!");
+    this._elements.push(newProsody);
+    return this;
+}
+
+/**
+ * @method audio
+ * @source {String} A URI to a valid audio resource. Optional.
+ * @param {Object} An object containing options.
+ * @throws {Error} The options provided are invalid.
+ */
+SSML.prototype.audio = function audio(source, options) {
+    if (_.isObject(source) && !_.isObject(options)) {
+        options = source;
+    } else if (!options) {
+        options = {};
+    }
+    if (_.isString(source)) {
+        options.src = source;
+    }
+    var newAudio = new Audio(options);
+    if (!newAudio.isValid) throw new Error("Audio has invalid options!");
+    this._elements.push(newAudio);
+    return this;
+}
+
+/**
+ * @method up
+ */
+SSML.prototype.up = function up() {
+    this._elements.push(new EndElement());
+    return this;
+}
+
+/**
+ * @method clear
+ */
+SSML.prototype.clear = function clear() {
+    this._elements = [];
+    return this;
+}
+
+function replace(keyValuePairs, replaceWith) {
+    if (_.isObject(keyValuePairs)) {
+        _.extend(this._replace, keyValuePairs);
+    } else if (_.isString(keyValuePairs) && _.isString(replaceWith)) {
+        var replacedWord = keyValuePairs;
+        var addendum = {};
+        addendum[replacedWord] = replaceWith;
+        _.extend(this._replace, addendum);
+    } else {
+        throw new Error('Invalid arguments for replace.');
+    }
+    return this;
+}
+
+/**
+ * @method replace
+ * @param {Object/String} A set of key-value pairs, or the word to be replaced. If the latter, the second parameter MUST be filled.
+ * @param {String} The word to replace the first one with. Only applicable if the first parameter is a string.
+ */
+SSML.prototype.replace = replace;
+
+/**
+ * @method addReplace
+ * @param {Object/String} A set of key-value pairs, or the word to be replaced. If the latter, the second parameter MUST be filled.
+ * @param {String} The word to replace the first one with. Only applicable if the first parameter is a string.
+ */
+SSML.prototype.addReplace = replace;
+
+/**
+ * @method removeReplace
+ * @param {String/Array} Either string or array of strings representing the keywords to be removed.
+ */
+SSML.prototype.removeReplace = function removeReplace(items) {
+    if (_.isArray(items)) {
+        for (var i = 0; i < items.length; i++) {
+            delete this._replace[items[i]];
+        }
+    } else {
+        delete this._replace[items];
+    }
+    return this;
+}
+
+/**
+ * @method clearReplace
+ */
+SSML.prototype.clearReplace = function clearReplace() {
+    this._replace = {};
+    return this;
+}
+
+/**
+ * @method toString
+ * @param {Object} An object containing options. Options are similar to those used by xmlBuilder.end().
+ *                 Object can also contain 2 more options:
+ *                 * minimal: If set to true, the resulting XML will have no headers, nor will the root element have attributes required for a valid SSML document.
+ *                 * full: If set to true, the root element will include optional XML attributes, such as xsi:schemaLocation.
+ */
+SSML.prototype.toString = function toString(options) {
+    var element;
+    if (options && options.minimal)
+        element = xmlBuilder.create('speak', null, null, { headless: true });
+    else {
+        element = xmlBuilder.create('speak');
+        element.att('xmlns', 'http://www.w3.org/2001/10/synthesis');
+        element.att('version', '1.0');
+        element.att('xml:lang','en-US');
+        if (options && options.full) {
+            element.att('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+            element.att('xsi:schemaLocation', 'http://www.w3.org/TR/speech-synthesis/synthesis.xsd');
+        }
+    }
+    for (var index = 0; index < this._elements.length; index++) {
+        var item = this._elements[index];
+        if (item.isValid()) {
+            element = item.renderInto(element, this._replace);
+        } else {
+            console.log('invalid element');
+        }
+    }
+    return element.end(options);
+}
+
+module.exports = SSML;
